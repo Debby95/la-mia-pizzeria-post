af7ea28edd85e79da1f734342eb41250e5b1f7e0
Debby95 <debbyjacksonstar@hotmail.it>
2022-12-01 13:22:54 +0100

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -0,0 +1,23 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            PizzeriaContext db = new PizzeriaContext();
+            List<Pizza> listaPizza = db.Pizzas.ToList();
+            return View(listaPizza);
+        }
+
+        public IActionResult Dettagli(int id)
+        {
+            PizzeriaContext db = new PizzeriaContext();
+            Pizza pizza = db.Pizzas.Where(p => p.Id == id).FirstOrDefault();
+            return View(pizza);
+        }
+    }
+}
---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Schema;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public float Price { get; set; }
+
+        
+
+        public Pizza(int id, string name, string description, string image, float price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+    }
+}
