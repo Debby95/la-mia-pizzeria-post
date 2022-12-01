fe0f90149d6db67c57cad29c915700f68443d41a
Debby95 <debbyjacksonstar@hotmail.it>
2022-12-01 17:26:22 +0100

    fine progetto

--- la-mia-pizzeria-static/Migrations/20221201161737_AddPizzaValidation.Designer.cs
+++ la-mia-pizzeria-static/Migrations/20221201161737_AddPizzaValidation.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using la_mia_pizzeria_static.Data;
 
@@ -10,9 +11,11 @@ using la_mia_pizzeria_static.Data;
 namespace lamiapizzeriastatic.Migrations
 {
     [DbContext(typeof(PizzeriaContext))]
-    partial class PizzeriaContextModelSnapshot : ModelSnapshot
+    [Migration("20221201161737_AddPizzaValidation")]
+    partial class AddPizzaValidation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- la-mia-pizzeria-static/Models/Pizza.cs
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -9,14 +9,17 @@ namespace la_mia_pizzeria_static.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "E' obbligatorio inserire il nome della pizza")]
+        [StringLength(50, ErrorMessage = "Il nome della pizza non può superare i 50 caratteri")]
         public string Name { get; set; }
 
         [Column(TypeName = "text")]
         [Required(ErrorMessage = "E' obbligatorio inserire la descrizione della pizza")]
+        [StringLength(400, ErrorMessage = "La descrizione della pizza non può superare i 400 caratteri")]
         public string Description { get; set; }
         public string Image { get; set; }
 
         [Required(ErrorMessage = "E' obbligatorio inserire il prezzo della pizza")]
+        [Range(1, 30, ErrorMessage = "Il prezzo della pizza non può essere minore di 1 o maggiore di 30.")]
         public float Price { get; set; }
 
         public Pizza()
